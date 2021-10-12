4ba3f654b3569ba5c28fd652ff4b40e1e0df683f
brmaciel <44143089+brmaciel@users.noreply.github.com>
2021-10-12 17:04:29 -0300

    Fixes in worker

--- Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Worker.swift
+++ Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Worker.swift
@@ -11,3 +11,9 @@ class ___VARIABLE_sceneName___Worker {
     
     
 }
+
+
+// MARK: - Access from Interactor
+extension ___VARIABLE_sceneName___Worker: ___VARIABLE_sceneName___WorkerProtocol {
+    
+}
