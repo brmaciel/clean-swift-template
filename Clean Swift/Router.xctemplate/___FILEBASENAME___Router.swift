06fa3d2ebb0f00621b5ae553ef96f1fbc8e57fbe
brmaciel <44143089+brmaciel@users.noreply.github.com>
2021-10-12 17:04:18 -0300

    Fixes in router

--- Clean Swift/Router.xctemplate/___FILEBASENAME___Router.swift
+++ Clean Swift/Router.xctemplate/___FILEBASENAME___Router.swift
@@ -12,60 +12,27 @@ class ___VARIABLE_sceneName___Router {
     weak var view: ___VARIABLE_sceneName___ViewController?
     
     
-    // MARK: Create Module
-    static func createModule(view: ___VARIABLE_sceneName___ViewController) {
+    // MARK: Create Scene
+    static func createScene() -> ___VARIABLE_sceneName___ViewController {
+        let view = UIStoryboard(name: "___VARIABLE_sceneName___", bundle: Bundle(for: ___VARIABLE_sceneName___ViewController.self)).instantiateInitialViewController() as! ___VARIABLE_sceneName___ViewController
         let interactor = ___VARIABLE_sceneName___Interactor()
         let presenter = ___VARIABLE_sceneName___Presenter()
+        let worker = ___VARIABLE_sceneName___Worker()
         let router = ___VARIABLE_sceneName___Router()
         
         view.interactor = interactor
         interactor.presenter = presenter
+        interactor.worker = worker
+        interactor.router = router
         presenter.view = view
-        presenter.router = router
         router.view = view
+        
+        return view
     }
     
+}
+
+// MARK: - Access from Interactor
+extension ___VARIABLE_sceneName___Router: ___VARIABLE_sceneName___RouterProtocol {
     
-    // MARK: - Routing
-
-//    func routeToSomewhere(segue: UIStoryboardSegue?) {
-//        if let segue = segue {
-//            let destinationVC = segue.destination as! SomewhereViewController
-//            var destinationDS = destinationVC.router!.dataStore!
-//            passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-//        } else {
-//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//            let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-//            var destinationDS = destinationVC.router!.dataStore!
-//            passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-//            navigateToSomewhere(source: viewController!, destination: destinationVC)
-//        }
-//    }
-
-    // MARK: - Navigation
-    
-//    func navigateToNextView(data: ___VARIABLE_sceneName___Models.ViewModel) {
-//        let destinationStoryboard = UIStoryboard(name: <#StoryboardFileName#>, bundle: Bundle.main)
-//
-//        let <#destinationVC#>: <#DestinationViewController#>
-//        if #available(iOS 13.0, *) {
-//            <#destinationVC#> = destinationStoryboard.instantiateViewController(identifier: <#identifierVC#>) as! <#DestinationViewController#>
-//        } else {
-//            <#destinationVC#> = destinationStoryboard.instantiateViewController(withIdentifier: <#identifierVC#>) as! <#DestinationViewController#>
-//        }
-//
-//        <#destination#>Router.createModule(view: <#destinationVC#>, data: data)
-//
-//        view?.navigationController?.pushViewController(statementsVC, animated: true)
-//    }
-
-//    func navigateToSomewhere(source: ___VARIABLE_sceneName___ViewController, destination: SomewhereViewController) {
-//        source.show(destination, sender: nil)
-//    }
-
-    // MARK: Passing data
-
-//    func passDataToSomewhere(source: ___VARIABLE_sceneName___DataStore, destination: inout SomewhereDataStore) {
-//        destination.name = source.name
-//    }
 }
--- Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Router.swift
+++ Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Router.swift
@@ -12,15 +12,17 @@ class ___VARIABLE_sceneName___Router {
     weak var view: ___VARIABLE_sceneName___ViewController?
     
     
-    // MARK: Create Module
-    static func createModule() -> UIViewController {
+    // MARK: Create Scene
+    static func createScene() -> ___VARIABLE_sceneName___ViewController {
         let view = UIStoryboard(name: "___VARIABLE_sceneName___", bundle: Bundle(for: ___VARIABLE_sceneName___ViewController.self)).instantiateInitialViewController() as! ___VARIABLE_sceneName___ViewController
         let interactor = ___VARIABLE_sceneName___Interactor()
         let presenter = ___VARIABLE_sceneName___Presenter()
+        let worker = ___VARIABLE_sceneName___Worker()
         let router = ___VARIABLE_sceneName___Router()
         
         view.interactor = interactor
         interactor.presenter = presenter
+        interactor.worker = worker
         interactor.router = router
         presenter.view = view
         router.view = view
@@ -28,9 +30,9 @@ class ___VARIABLE_sceneName___Router {
         return view
     }
     
-    
-    // MARK: - Routing
-    
-    
+}
+
+// MARK: - Access from Interactor
+extension ___VARIABLE_sceneName___Router: ___VARIABLE_sceneName___RouterProtocol {
     
 }
